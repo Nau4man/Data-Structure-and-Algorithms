b1d9709b03038b8bcde3c961de0947b4c8c98509
Nauman Sabir <nauman.sabir@wecrunch.com>
2020-06-24 02:13:12 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+class PythonCodingExample:
+
+    # Login Implementation using If else
+
+
+	def login(self, email, password):
+		if email == "nomi@email.com" and password == 1234567:
+			print("You're Logged In! Welcome")
+		else:
+			print("Sorry, Incorrect Credentials")
+
+
+	email = input("Enter your Email:")
+	password = input("Enter your Password:")
+
+	# login(email,password)
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	if __name__ == "__main__":
+		pass
