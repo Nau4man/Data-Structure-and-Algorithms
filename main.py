07aa789c37d01b3c1be77d513ee3d1a68948a271
Nauman Sabir <nauman.sabir@wecrunch.com>
2020-07-05 23:40:18 +0500

    Added Resigter User Functionality
    Hey Red, I messed up and now there is a bug in the login fucntion.  Can you please fix it for me?

--- main.py
+++ main.py
@@ -8,7 +8,21 @@ def login(email, password):
         print("Sorry, Incorrect Credentials")
 
 
-email = input("Enter your Email:")
-password = input("Enter your Password:")
+def register(name,email,password):
 
-login(email, password)
+	return "Welcome "+name+"\n Please note down your email and Password to login: \n Email: "+email+"\n Password: "+password
+
+	
+
+
+def main():
+	name = input("Enter your Name:")
+	email = input("Enter your Email:")
+	password = input("Enter your Password:")
+	print(register(name, email, password))
+	email = input("Enter your Email:")
+	password = input("Enter your Password:")
+	login(name, email)
+# login(email, password)
+if __name__ == '__main__':
+	main()
