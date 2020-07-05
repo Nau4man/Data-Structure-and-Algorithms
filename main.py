ede46a95489c5f11747d1423592342cbfd9a24b4
rediet <mulunehrediet@gmail.com>
2020-07-05 11:00:39 -0700

    changed email
    I change your email to my email.

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-
-
-    # Login Implementation using If else
+# Login Implementation using If else
 
 
 def login(email, password):
-	if email == "nomi@email.com" and password == 1234567:
-		print("You're Logged In! Welcome")
-	else:
-		print("Sorry, Incorrect Credentials")
+    if email == "mulunehrediet@gmail.com" and password == 1234567:
+        print("You're Logged In! Welcome")
+    else:
+        print("Sorry, Incorrect Credentials")
 
 
 email = input("Enter your Email:")
 password = input("Enter your Password:")
 
-login(email,password)
-
-
-	
-
-
-
-
+login(email, password)
