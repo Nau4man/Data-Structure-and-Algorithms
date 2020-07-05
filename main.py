3f8a33e7040288963de55f125dcfbb4dfb46c74d
Nauman Sabir <nauman.sabir@wecrunch.com>
2020-07-06 00:01:20 +0500

    Made an Authentication Class

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# Login Implementation using If else
+class Authentication:
 
+	def __init__(self, name, email, password):
+	    self.name = name
+	    self.email = email
+	    self.password = password
 
-def login(email, password):
-    if email == "mulunehrediet@gmail.com" and password == 1234567:
-        print("You're Logged In! Welcome")
-    else:
-        print("Sorry, Incorrect Credentials")
+	# Login Implementation using If else
 
+	def login(email, password):
+	    if email == "mulunehrediet@gmail.com" and password == 1234567:
+	        print("You're Logged In! Welcome")
+	    else:
+	        print("Sorry, Incorrect Credentials")
 
-def register(name,email,password):
+	def register(name,email,password):
+		print(name)
 
-	return "Welcome "+name+"\n Please note down your email and Password to login: \n Email: "+email+"\n Password: "+password
+		# return "Welcome "+name+"\n Please note down your email and Password to login: \n Email: "+email+"\n Password: "+password
 
 	
 
@@ -19,10 +25,11 @@ def main():
 	name = input("Enter your Name:")
 	email = input("Enter your Email:")
 	password = input("Enter your Password:")
-	print(register(name, email, password))
+	auth = Authentication(name, email, password)
+	print(auth.register(name, email,password))
 	email = input("Enter your Email:")
 	password = input("Enter your Password:")
-	login(name, email)
-# login(email, password)
+	auth.login(name, email)
+	
 if __name__ == '__main__':
 	main()
